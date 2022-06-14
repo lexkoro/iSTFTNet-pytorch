931f59cd704c4de744f44438aa620d73245dcf8c
Alexander Korolev <alexx.korolev@gmail.com>
2022-06-14 10:50:14 +0200

    add spec loss

---
+++ stft_loss.py
@@ -0,0 +1,23 @@
+"""STFT-based Loss modules."""
+
+import torch
+import torch.nn.functional as F
+
+
+def stft(x, fft_size, hop_size, win_length, window):
+    """Perform STFT and convert to magnitude spectrogram.
+    Args:
+        x (Tensor): Input signal tensor (B, T).
+        fft_size (int): FFT size.
+        hop_size (int): Hop size.
+        win_length (int): Window length.
+        window (str): Window function type.
+    Returns:
+        Tensor: Magnitude spectrogram (B, #frames, fft_size // 2 + 1).
+    """
+    x_stft = torch.stft(x, fft_size, hop_size, win_length, window)
+    real = x_stft[..., 0]
+    imag = x_stft[..., 1]
+
+    # NOTE(kan-bayashi): clamp is needed to avoid nan or inf
+    return torch.sqrt(torch.clamp(real**2 + imag**2, min=1e-7)).transpose(2, 1)
--- train.py
+++ train.py
@@ -19,6 +19,7 @@ from models import (
     Generator,
     MultiPeriodDiscriminator,
     MultiScaleDiscriminator,
+    MultiResSpecDiscriminator,
     feature_loss,
     generator_loss,
     discriminator_loss,
@@ -44,6 +45,8 @@ def train(rank, a, h):
     generator = Generator(h).to(device)
     mpd = MultiPeriodDiscriminator().to(device)
     msd = MultiScaleDiscriminator().to(device)
+    mrsd = MultiResSpecDiscriminator().to(device)
+
     stft = STFT(filter_length=16, hop_length=8, win_length=16).to(device)
 
     if rank == 0:
@@ -65,6 +68,7 @@ def train(rank, a, h):
         generator.load_state_dict(state_dict_g["generator"])
         mpd.load_state_dict(state_dict_do["mpd"])
         msd.load_state_dict(state_dict_do["msd"])
+        mrsd.load_state_dict(state_dict_do["mrsd"])
         steps = state_dict_do["steps"] + 1
         last_epoch = state_dict_do["epoch"]
 
@@ -72,12 +76,13 @@ def train(rank, a, h):
         generator = DistributedDataParallel(generator, device_ids=[rank]).to(device)
         mpd = DistributedDataParallel(mpd, device_ids=[rank]).to(device)
         msd = DistributedDataParallel(msd, device_ids=[rank]).to(device)
+        mrsd = DistributedDataParallel(mrsd, device_ids=[rank]).to(device)
 
     optim_g = torch.optim.AdamW(
         generator.parameters(), h.learning_rate, betas=[h.adam_b1, h.adam_b2]
     )
     optim_d = torch.optim.AdamW(
-        itertools.chain(msd.parameters(), mpd.parameters()),
+        itertools.chain(msd.parameters(), mpd.parameters(), mrsd.parameters()),
         h.learning_rate,
         betas=[h.adam_b1, h.adam_b2],
     )
@@ -159,6 +164,7 @@ def train(rank, a, h):
     generator.train()
     mpd.train()
     msd.train()
+    mrsd.train()
     for epoch in range(max(0, last_epoch), a.training_epochs):
         if rank == 0:
             start = time.time()
@@ -204,8 +210,13 @@ def train(rank, a, h):
             loss_disc_s, losses_disc_s_r, losses_disc_s_g = discriminator_loss(
                 y_ds_hat_r, y_ds_hat_g
             )
+            # MRSD
+            y_dMRS_hat_r, y_dMRS_hat_g, _, _ = mrsd(y, y_g_hat.detach())
+            loss_disc_MRS, losses_disc_s_r, losses_disc_s_g = discriminator_loss(
+                y_dMRS_hat_r, y_dMRS_hat_g
+            )
 
-            loss_disc_all = loss_disc_s + loss_disc_f
+            loss_disc_all = loss_disc_s + loss_disc_f + loss_disc_MRS
 
             loss_disc_all.backward()
             optim_d.step()
@@ -218,11 +229,23 @@ def train(rank, a, h):
 
             y_df_hat_r, y_df_hat_g, fmap_f_r, fmap_f_g = mpd(y, y_g_hat)
             y_ds_hat_r, y_ds_hat_g, fmap_s_r, fmap_s_g = msd(y, y_g_hat)
+            y_dMRS_hat_r, y_dmrs_hat_g, fmap_mrs_r, fmap_mrs_g = mrsd(y, y_g_hat)
+
             loss_fm_f = feature_loss(fmap_f_r, fmap_f_g)
             loss_fm_s = feature_loss(fmap_s_r, fmap_s_g)
+            loss_fm_mrs = feature_loss(fmap_mrs_r, fmap_mrs_g)
             loss_gen_f, losses_gen_f = generator_loss(y_df_hat_g)
             loss_gen_s, losses_gen_s = generator_loss(y_ds_hat_g)
-            loss_gen_all = loss_gen_s + loss_gen_f + loss_fm_s + loss_fm_f + loss_mel
+            loss_gen_mrs, losses_gen_mrs = generator_loss(y_dmrs_hat_g)
+            loss_gen_all = (
+                loss_gen_s
+                + loss_gen_f
+                + loss_gen_mrs
+                + loss_fm_s
+                + loss_fm_mrs
+                + loss_fm_f
+                + loss_mel
+            )
 
             loss_gen_all.backward()
             optim_g.step()
